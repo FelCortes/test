946a81c62d1fdf677e3bc3ea3003cc1d6371c52e
DiegoRosas-py <diego.rosas0168@gmail.com>
2025-06-19 02:10:50 -0400

    fix errors

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn --bind=0.0.0.0:8000 --workers=4 banco_mgti.wsgi
+gunicorn --bind=0.0.0.0:8000 --workers=2 banco_mgti.wsgi
